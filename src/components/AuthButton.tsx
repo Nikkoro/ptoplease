65974864e5eaa8b19565b03d4e4e75e7cc623e80
nikkoro <nikodemdomaracki@gmail.com>
2024-01-30 01:34:27 +0100

    setup nextjs with shadcn & supabase

---
+++ src/components/AuthButton.tsx
@@ -0,0 +1,41 @@
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+export default async function AuthButton() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const signOut = async () => {
+    "use server";
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      <span className="text-sm font-medium hover:underline underline-offset-4">
+        Hey, {user.email}!
+      </span>
+      <form action={signOut}>
+        <Button>Logout</Button>
+      </form>
+    </div>
+  ) : (
+    <Link
+      href="/login"
+      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+    >
+      Login
+    </Link>
+  );
+}
